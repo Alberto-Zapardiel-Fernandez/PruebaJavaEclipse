b2906e7cebcc996e1e3d3c0cb3053a0e951e6c5e
sinensia <sinensia@192.168.1.17>
2023-05-23 12:53:04 +0200

    Clase iniciadora de la aplicación

---
+++ Ej_05CentralitaTelefonica/src/es/curso/directora/CentralitaTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package es.curso.directora;
+
+import es.curso.model.Centralita;
+
+/**
+ * @author Alberto Zapardiel Fernández
+ *
+ */
+
+public class CentralitaTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Centralita centralita = new Centralita();
+		centralita.contarLlamadas();
+	}
+
+}
